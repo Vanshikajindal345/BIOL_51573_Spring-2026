2ffe26608e2be06aaddf4a021b10f5eb395856bf
Vanshika Jindal <vanshikajindal01@gmail.com>
2026-03-04 10:55:59 -0600

    updated folder

--- scripts/hello_worldVS.py
+++ scripts/hello_worldVS.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
+#prompt the user for their name
+name = input ("Please enter your name:")
+
 print("hello world")
 print("hello world")
 
 print("hello, ", name, " I hope you have a great day.")
+
+#some math
+print(7+7*8)
